71d25de498abcab938fd4aaece9ea69ae1d56b95
samonide <emamulmontachirsajid@gmail.com>
2024-11-12 23:29:56 +0600

    neovim conf

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,102 @@
+return {
+  'hrsh7th/nvim-cmp',
+  event = "InsertEnter",
+  dependencies = {
+    {
+      "L3MON4D3/LuaSnip",
+      dependencies = "rafamadriz/friendly-snippets",
+      opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+    },
+    {
+      "windwp/nvim-autopairs",
+      event = "InsertEnter",
+      opts = {
+        fast_wrap = {},
+        disable_filetype = { "TelescopePrompt", "vim" },
+      },
+      config = function(_, opts)
+        require("nvim-autopairs").setup(opts)
+
+        -- setup cmp for autopairs
+        local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+        require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+      end,
+    },
+    {
+      'nmac427/guess-indent.nvim',
+      config = function()
+        require('guess-indent').setup({
+          auto_cmd = true, -- Set to false to disable automatic execution
+        })
+      end
+    },
+    {
+      "lukas-reineke/indent-blankline.nvim",
+      main = "ibl",
+      opts = {},
+      config = function()
+        require("ibl").setup({
+          indent = {
+            char = "▏",
+          },
+        })
+      end
+    },
+    'saadparwaiz1/cmp_luasnip',
+    'hrsh7th/cmp-nvim-lua',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+  },
+  config = function()
+    local cmp = require("cmp")
+
+    require("luasnip.loaders.from_vscode")
+
+    cmp.setup({
+      experimental = {
+        ghost_text = true,
+      },
+      performance = {
+        debounce = 60;
+        fetchingTimeout = 200;
+        max_view_entries = 30;
+      };
+      window = {
+        completion = cmp.config.window.bordered({
+          border = 'rounded',
+          scrollbar = false,
+          winhighlight = 'Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None',
+        }),
+        documentation = cmp.config.window.bordered({
+          border = "rounded",
+        }),
+      },
+      formatting = {
+        fields = { "kind", "abbr", "menu" },
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-o>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<TAB>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+        ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+      }),
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+      },
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = "nvim_lua" },
+        { name = 'luasnip' },
+        { name = 'buffer' },
+        { name = 'path' },
+        { name = "crates" },
+      }),
+    })
+  end,
+}
